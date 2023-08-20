c7807504ecce6caf216fdbbad06640cc3ffbfcb4
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-08-20 13:45:49 +0400

    Update

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        int roundedNumber = Math.round(numberToBeRounded);
+        System.out.println(roundedNumber);
+    }
+
+    public static void main(String[] args) {
+        FloatTypeCasting floatTypeCasting = new FloatTypeCasting();
+        float numberToBeRounded = 12.345f;
+        floatTypeCasting.roundNumber(numberToBeRounded);
     }
 }
